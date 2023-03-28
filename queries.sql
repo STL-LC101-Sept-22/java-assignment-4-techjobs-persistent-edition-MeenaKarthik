6df05f7f753b7ef2d9d506f4750d721f862ad4cf
Meena Sundaram <meena@Meenas-Laptop.local>
2023-03-27 20:11:53 -0500

    tech jobs GA work done

--- queries.sql
+++ queries.sql
@@ -1,7 +1,12 @@
 -- Part 1: Test it with SQL
+--SELECT COLUMN_NAME,DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'job'
+
 
 -- Part 2: Test it with SQL
+--select name from employer where location = "St. Louis City";
 
 -- Part 3: Test it with SQL
+--drop table job;
 
 -- Part 4: Test it with SQL
+--SELECT * FROM skill INNER JOIN job_skills ON skill.id = job_skills.skills_id WHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,41 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min=3,max=15,message = "size must be between 3 and 15")
+    private String location;
 
+    //@OneToMany(mappedBy = "employer")
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public Employer() {}
 }
